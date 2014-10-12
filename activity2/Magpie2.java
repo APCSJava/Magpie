daf7ee67e415c4a1b817f4fe0d95d1ec7c6bddf1
rebecca-brunsberg <rebecca_brunsberg@asl.org>
2014-10-12 14:24:54 +0100

    Updated authors

--- activity2/Magpie2.java
+++ activity2/Magpie2.java
@@ -8,7 +8,8 @@
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
- * @version April 2012
+ * @author Rebecca Brunsberg and Roxy Sammons
+ * @version October 2014
  */
 public class Magpie2
 {
