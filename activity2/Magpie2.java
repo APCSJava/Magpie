c3af5e0f534582a8f0878fefd0e92084c3d0146f
sophiazorek <sophia_zorek@asl.org>
2014-10-13 21:11:38 +0100

    Update Magpie2.java

--- activity2/Magpie2.java
+++ activity2/Magpie2.java
@@ -4,79 +4,108 @@
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ *       Handles responding to simple words and phrases 
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
  * @version April 2012
  */
-public class Magpie2
-{
-	/**
-	 * Get a default greeting 	
-	 * @return a greeting
-	 */
-	public String getGreeting()
+import java.util.Scanner;
+public class Magpie2 {
+    public static void main(String[] args)
 	{
-		return "Hello, let's talk.";
-	}
-	
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
-		else
-		{
-			response = getRandomResponse();
-		}
-		return response;
-	}
-
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
-		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-		String response = "";
+		Magpie2 maggie = new Magpie2();
 		
-		if (whichResponse == 0)
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.equals("Bye"))
 		{
-			response = "Interesting, tell me more.";
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
 		}
-		else if (whichResponse == 1)
-		{
-			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
-			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
-			response = "You don't say.";
-		}
-
-		return response;
 	}
+  /**
+   * Get a default greeting  
+   * @return a greeting
+   */
+  public String getGreeting()
+  {
+    return "Hello, let's talk.";
+  }
+  /**
+   * Gives a response to a user statement
+   * 
+   * @param statement
+   *            the user statement
+   * @return a response based on the rules given
+   */
+  public String getResponse(String statement)
+  {
+    String response = "";
+    if (statement.indexOf("no") >= 0){
+      response = "Why so negative?";
+      
+    } else if (statement.indexOf("mother") >= 0
+                 || statement.indexOf("father") >= 0
+                 || statement.indexOf("sister") >= 0
+                 || statement.indexOf("brother") >= 0)
+    {
+      response = "Tell me more about your family.";
+      
+    } else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >=  0) {
+      System.out.println ("Tell me more about your pets.");
+      
+    } else if (statement.indexOf("Mr.") >= 0) { 
+      System.out.println ("He seems like a good teacher.");
+      
+    } else if (statement.indexOf("Ms.") >=  0 || statement.indexOf("Mrs.") >=  0) {
+      System.out.println ("She seems like a good teacher.");
+      
+    }else if (statement.indexOf("school") >= 0) {
+      System.out.println("Tell me more about school?");
+    } else if (statement.indexOf("sleep") >= 0) {
+      System.out.println("How much sleep do you get?");
+    } else if (statement.indexOf("computer science") >= 0) {
+      System.out.println("Computer science is awesome!"); } 
+    else if (statement.indexOf("") >= 0) {
+      System.out.println ("Say something, please.");
+
+    } else {
+      
+      response = getRandomResponse();
+    }
+    return response;
+  }
+  /**
+   * Pick a default response to use if nothing else fits.
+   * @return a non-committal string
+   */
+  private String getRandomResponse()
+  {
+    final int NUMBER_OF_RESPONSES = 4;
+    double r = Math.random();
+    int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+    String response = "";
+    
+    if (whichResponse == 0)
+    {
+      response = "Interesting, tell me more.";
+    }
+    else if (whichResponse == 1)
+    {
+      response = "Hmmm.";
+    }
+    else if (whichResponse == 2)
+    {
+      response = "Do you really think so?";
+    }
+    else if (whichResponse == 3)
+    {
+      response = "You don't say.";
+    }
+    
+    return response;
+  }
 }
