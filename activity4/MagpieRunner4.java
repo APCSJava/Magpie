a3011bd98c1bd2575ca25f460fd2550e358b492e
Jordi Albane <jordi_albanell@asl.org>
2014-10-16 21:51:11 +0100

    Sorry this is final version

--- activity4/MagpieRunner4.java
+++ activity4/MagpieRunner4.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 /**
  * A simple class to run the Magpie class.
  * @author Laurie White
- * @version April 2012
+ * @author Jordi Albanell
+ * @version October 2014
  */
 public class MagpieRunner4
 {
 
-	/**
-	 * Create a Magpie, give it user input, and print its replies.
-	 */
-	public static void main(String[] args)
-	{
-		Magpie4 maggie = new Magpie4();
-		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (maggie.getResponse(statement));
-			statement = in.nextLine();
-		}
-	}
+ /**
+  * Create a Magpie, give it user input, and print its replies.
+  */
+ public static void main(String[] args)
+ {
+  Magpie4 maggie = new Magpie4();
+  
+  System.out.println (maggie.getGreeting());
+  Scanner in = new Scanner (System.in);
+  String statement = in.nextLine();
+  
+  while (!statement.equals("Bye"))
+  {
+   System.out.println (maggie.getResponse(statement));
+   statement = in.nextLine();
+  }
+ }
 
 }
